34888921093e69edc07d99edaba4495e9a1cb253
Yupanqui Munoz <yupanqui.munoz@palico.com>
2023-03-03 15:32:08 +0100

    first commit

---
+++ src/main/java/org/munozy/arkadialibrarybo/ArkadiaLibraryBoApplication.java
@@ -0,0 +1,13 @@
+package org.munozy.arkadialibrarybo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkadiaLibraryBoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkadiaLibraryBoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/munozy/arkadialibrarybo/ArkadiaLibraryBoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.munozy.arkadialibrarybo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkadiaLibraryBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
